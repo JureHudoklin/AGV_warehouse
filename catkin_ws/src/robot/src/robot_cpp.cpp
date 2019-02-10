c0c9332273afbc01c3e898c759e6d9a4bfa4ec6c
SSCswist <32479419+SSCswist@users.noreply.github.com>
2019-02-10 18:08:00 +0100

    test

--- catkin_ws/src/robot/src/robot_cpp.cpp
+++ catkin_ws/src/robot/src/robot_cpp.cpp
@@ -1,3 +1,6 @@
+// http://mate.tue.nl/mate/pdfs/7566.pdf reference for robot controll
+// http://www-ist.massey.ac.nz/conferences/ICARA2004/files/Papers/Paper74_ICARA2004_425_428.pdf
+
 #include <ros/ros.h>
 
 #include <robotcontrol.h>
@@ -12,6 +15,19 @@
 #include <nav_msgs/Odometry.h>
 
 #include <std_srvs/Empty.h>
+
+struct robot_velocity_state {
+    double v_x; //Front left
+    double v_y; //Frpnt right
+	double a_z; //Back right
+};
+
+struct motor_power_state {
+    double pow_1; //Front left
+    double pow_2; //Frpnt right
+	double pow_3; //Back right
+	double pow_4; //Back left
+};
 
 //Publishers
 ros::Publisher pub;
@@ -32,7 +48,8 @@ ros::ServiceServer motors_off;
 
 //Define used variable
 bool motor_state;
-double v_x, v_y, a_z;
+robot_velocity_state robot_velocity;
+
 
 bool motors_on_call(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res) {
     motor_state = true;
@@ -43,11 +60,27 @@ bool motors_off_call(std_srvs::Empty::Request &req, std_srvs::Empty::Response &r
     motor_state = false;
     return true;
 }
+
 void twist_callback(const geometry_msgs::Twist &twist) {		//Zakaj kle const. Zakaj argument v fn ce ga spodi ne dam
-	v_x = twist.linear.x;
-	v_y = twist.linear.y;
-	a_z = twist.angular.z;
+	robot_velocity.v_x = twist.linear.x;
+	robot_velocity.v_y = twist.linear.y;
+	robot_velocity.a_z = twist.angular.z;
 	ROS_INFO("nove_hitrosti");
+}
+
+void vel2motor(&power) {
+	double vmesni_izracun[4];
+	double koti_koles[4] = [3.*M_PI/4., 5.*M_PI/4., 7.*M_PI/4., M_PI/4.];
+
+	for(int i = 0; i<4; i++) {
+		vmesni_izracun[i] = a_z + v_x * cos(koti_koles[i]) + v_y * sin(koti_koles[i]);
+	}
+
+	//TUKAJ DOADAJ DA BO SKALIRALO OUTPUT DO 1
+	power.pow_1 = vmesni_izracun[1]
+	power.pow_2 = vmesni_izracun[2]
+	power.pow_3 = vmesni_izracun[3]
+	power.pow_4 = vmesni_izracun[4]
 }
 
 
@@ -59,13 +92,14 @@ int main(int argc, char** argv) {
 	ros::NodeHandle nh;
 
     //Start adverstisers, subscribers and services
-    pub = nh.advertise<std_msgs::Int32>("jure_topic", 1000);
+    //pub = nh.advertise<std_msgs::Int32>("jure_topic", 1000);
     //sub = nh.subscribe("tema_subscriptiona", 1000, counterCallback); 
+
+
 	motors_on = nh.advertiseService("/motors_on", motors_on_call);
 	motors_off = nh.advertiseService("/motors_off", motors_off_call);
 
 
-	std_msgs::Int32 spremenljivka;
 	spremenljivka.data = 0;
 	ros::Rate loop_rate(2);
 
@@ -90,6 +124,11 @@ int main(int argc, char** argv) {
 			ROS_ERROR_STREAM("Encoder initialization unsucessfull");
 			return -1;
 		}
+		for(int i = 0; i<4; i++) {
+			if(rc_encoder_write(i, 0) == -1) {
+				ROS_ERROR_STREAM("Encoder %d set 0 unsucessfull", i);
+			}
+		}
 	}
 
 
@@ -97,15 +136,15 @@ int main(int argc, char** argv) {
 
 	while (ros::ok())
 	{
-
 		if (motor_state){
-			double pow_1, pow_2, pow_3, pow_4;  //Based on v_x,v_y,a_z set power for each motor
+			motor_power power;  //Based on v_x,v_y,a_z set power for each motor
 
+			vel2motor(power);
 
-			rc_motor_set(1, pow_1);
-			rc_motor_set(2, pow_2);
-			rc_motor_set(3, pow_3);
-			rc_motor_set(4, pow_4);
+			rc_motor_set(1, power.pow_1);
+			rc_motor_set(2, power.pow_2);
+			rc_motor_set(3, power.pow_3);
+			rc_motor_set(4, power.pow_4);
 			ROS_INFO(" %f ", v_x);
 		}
 		else {
@@ -115,12 +154,14 @@ int main(int argc, char** argv) {
 			rc_motor_set(4,0);
 			ROS_INFO("ne_dela");
 		}
-		pub.publish(spremenljivka);
-		//ROS_INFO("Help me with cpp");
-
 
 		ros::spinOnce();
 		loop_rate.sleep();
+	}
+
+	if (use_motors){
+		rc_motor_cleanup();
+		rc_encoder_cleanup();
 	}
 	return 0;
 }
